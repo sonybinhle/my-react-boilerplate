945b53172b090dc5ee4a960f49817ecc9df51bcf
Binh Le <sonybinhle@gmail.com>
2018-02-21 09:33:54 +0100

    #6: Fix test on version

---
+++ src/versions/vio/components/Logo/index.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { Link } from 'react-router-dom';
+
+import Logo from './';
+
+describe('vio:Logo', () => {
+  it('Should render Logo without error', () => {
+    const wrapper = shallow(<Logo />);
+
+    expect(wrapper.find(Link).length).toBe(1);
+  });
+});
